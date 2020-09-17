40ac3a2ae40f406648be6ae2bae808657f8ee965
jessicarf18 <jessicarosalia678@gmail.com>
2020-09-17 10:51:35 -0300

    criando class bd e indices dinamicos

--- js/app.js
+++ js/app.js
@@ -9,6 +9,32 @@ class Despesa {
     }
 }
 
+class Bd {
+    constructor() {
+        let id = localStorage.getItem('id')
+
+        if (id === null) {
+            localStorage.setItem('id', 0)
+        }
+    }
+
+    getProximoId() {
+        let proximoId = localStorage.getItem('id')
+        return parseInt(proximoId) + 1
+    }
+
+    gravar(d) {
+        //acessar local storage
+        let id = this.getProximoId()
+
+        localStorage.setItem(id, JSON.stringify(d))
+
+        localStorage.setItem('id', id)
+    }
+}
+
+let bd = new Bd()
+
 function cadastrarDespesa() {
     const year = document.querySelector("#year")
     const month = document.querySelector("#month")
@@ -20,5 +46,5 @@ function cadastrarDespesa() {
     let despesa = new Despesa(year.value, month.value,
         day.value, type.value, description.value, value.value)
 
-    console.log(despesa)
+    bd.gravar(despesa)
 }
